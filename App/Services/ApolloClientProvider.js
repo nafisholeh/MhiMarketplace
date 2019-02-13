0faf9f1398d9553a29b1f1b9d0ae826fd538fcd7
Rachmad <rachmadnafisholeh@gmail.com>
2019-02-13 07:49:33 +0700

    chore(graphql): setup graphql

---
+++ App/Services/ApolloClientProvider.js
@@ -0,0 +1,25 @@
+import ApolloClient from 'apollo-boost'
+import { NavigationActions } from 'react-navigation'
+var _ = require('lodash')
+
+class ApolloClientProvider {
+
+  constructor() {
+    this.client = new ApolloClient({
+      uri: 'http://localhost:4000/graphql/',
+      onError: this._onError,
+    })
+  }
+
+  _onError = (errorObj) => {
+    this._onHandleUnauthorized(errorObj)
+  }
+
+  // handle jika ada error sebab unauthorized
+  _onHandleUnauthorized(error) {
+    if(_.isNil(error)) return
+  }
+
+}
+
+export default new ApolloClientProvider()

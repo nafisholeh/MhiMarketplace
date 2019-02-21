412a629606b6d9f47e301781bb73bb3f59eba18f
Rachmad <rachmadnafisholeh@gmail.com>
2019-02-21 23:29:29 +0700

    feat(product): display restructured scheme on product list

--- App/Lib/money.js
+++ App/Lib/money.js
@@ -1,6 +1,7 @@
 var _ = require('lodash');
 
 export function parseToRupiah(angka, prefixTerm) {
+  if(angka) {
     let rupiah = '';
     //split behind comma
     let frontNumber = 0
@@ -11,9 +12,24 @@ export function parseToRupiah(angka, prefixTerm) {
         afterComma=','+afterComma
     }
     frontNumber=fullNumber[0]
-	let angkarev = frontNumber.toString().split('').reverse().join('');
-    for(let i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
-  let output = rupiah.split('',rupiah.length-1).reverse().join('')+(afterComma>0?afterComma:'')
-  if(!_.isNil(prefixTerm)) return prefixTerm+output;
-  else return 'Rp '+output;
+  	let angkarev = frontNumber.toString().split('').reverse().join('');
+      for(let i = 0; i < angkarev.length; i++) if(i%3 == 0) rupiah += angkarev.substr(i,3)+'.';
+    let output = rupiah.split('',rupiah.length-1).reverse().join('')+(afterComma>0?afterComma:'')
+    if(!_.isNil(prefixTerm)) return prefixTerm+output;
+    else return 'Rp '+output;
+  }
+  return null;  
+}
+
+/*
+ * calculate discount price
+ * param price number
+ * param discount number
+ * return discount_price number
+*/
+export function calcDiscount(price, discount) {
+  if(price && discount) {
+    return price - (price * ( discount / 100));
+  }
+  return null;
 }

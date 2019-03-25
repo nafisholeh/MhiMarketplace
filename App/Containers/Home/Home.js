c14f05d888cb4b2a8f93222c2a33494e7623e720
Rachmad <rachmadnafisholeh@gmail.com>
2019-03-25 21:17:38 +0700

    chore: setup eslint and prettier on project

--- App/Containers/Home/Home.js
+++ App/Containers/Home/Home.js
@@ -1,61 +1,70 @@
-import React, { Component } from 'react'
-import { ScrollView, Text, Image, View, TouchableOpacity } from 'react-native'
-import { Query } from 'react-apollo'
+import React, { Component } from 'react';
+import { Text, Image, View, TouchableOpacity } from 'react-native';
+import { Query } from 'react-apollo';
+import { shape } from 'prop-types';
 
-import { FETCH_PRODUCT_LIST } from './GraphQL/Query'
-import { Images, Metrics } from 'Themes'
-import { parseToRupiah, calcDiscount } from 'Lib'
-import { OptimizedList } from 'Components'
-import styles from './Styles'
-    
+import { Metrics } from 'Themes';
+import { parseToRupiah, calcDiscount } from 'Lib';
+import { OptimizedList } from 'Components';
+
+import { FETCH_PRODUCT_LIST } from './GraphQL/Query';
+import styles from './Styles';
+
 export default class Home extends Component {
-  
-  _onClickProduct = (data) => {
-    this.props.navigation.navigate('Detail', { data })
-  }
-  
-  _renderRow = (type, data) => {
-    return (
-      <TouchableOpacity 
-        onPress={() => this._onClickProduct(data)}
-        style={styles.product__item}
-        >
-        <View style={styles.product__item_content}>
-          <Image source={{uri: data.photo }} style={{width:60, height:60}}/>
-          <View style={{flexDirection:'column', justifyContent: 'center'}}>
-            <Text style={{textAlign:'right', fontWeight:'bold'}}>{data.title}</Text>
-            <Text style={{textAlign:'right'}}>{parseToRupiah(data.price)}</Text>
-            <Text style={{textAlign:'right'}}>{parseToRupiah(calcDiscount(data.price, data.discount))}</Text>
-          </View>
+  _onClickProduct = data => {
+    const { navigation } = this.props;
+    navigation.navigate('Detail', { data });
+  };
+
+  renderRow = (type, data) => (
+    <TouchableOpacity
+      onPress={() => this._onClickProduct(data)}
+      style={styles.product__item}
+    >
+      <View style={styles.product__item_content}>
+        <Image source={{ uri: data.photo }} style={{ width: 60, height: 60 }} />
+        <View style={{ flexDirection: 'column', justifyContent: 'center' }}>
+          <Text style={{ textAlign: 'right', fontWeight: 'bold' }}>
+            {data.title}
+          </Text>
+          <Text style={{ textAlign: 'right' }}>
+            {parseToRupiah(data.price)}
+          </Text>
+          <Text style={{ textAlign: 'right' }}>
+            {parseToRupiah(calcDiscount(data.price, data.discount))}
+          </Text>
         </View>
-      </TouchableOpacity>
-    )
-  }
-  
-  render () {
+      </View>
+    </TouchableOpacity>
+  );
+
+  render() {
+    // console.tron.log(`test ${ parseToRupiah('1000000')}`);
     return (
       <View style={styles.container}>
-
-        <View style={styles.section} >
+        <View style={styles.section}>
           <Query query={FETCH_PRODUCT_LIST}>
             {({ loading, error, data, refetch }) => {
-              if(error) {
-                return (<View></View>)
-              } else if(data) {
+              if (error) {
+                return <View />;
+              } else if (data) {
                 return (
                   <OptimizedList
                     itemWidth={Metrics.deviceWidth}
                     itemHeight={100}
-                    data={data.products} 
-                    renderRow={this._renderRow}
+                    data={data.products}
+                    renderRow={this.renderRow}
                   />
-                )
+                );
               }
             }}
           </Query>
         </View>
-        
       </View>
-    )
+    );
   }
 }
+
+Home.propTypes = {
+  navigation: shape({}),
+};

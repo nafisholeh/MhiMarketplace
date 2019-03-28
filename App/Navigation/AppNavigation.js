6317e24688d0a4d30140c7ee966375909c757b5e
Rachmad <rachmadnafisholeh@gmail.com>
2019-03-28 12:19:56 +0700

    feat(nav): setup signin and tab navigation

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,17 +1,15 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation'
-import Home from '../Containers/Home/Home'
-import Detail from '../Containers/Product/Detail'
+import Signin from '../Containers/Signin/Signin'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = createStackNavigator({
-  Home: { screen: Home },
-  ProductDetail: { screen: Detail }
+  Signin: { screen: Signin },
 }, {
   // Default config for all screens
   headerMode: 'none',
-  initialRouteName: 'Home',
+  initialRouteName: 'Signin',
   navigationOptions: {
     headerStyle: styles.header
   }

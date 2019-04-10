9b395b58fde6309a1580230a5bb1871f69b24d55
Rachmad <rachmadnafisholeh@gmail.com>
2019-04-11 05:22:32 +0700

    feat(graphql): setup cache deps

--- App/Services/ApolloClientProvider.js
+++ App/Services/ApolloClientProvider.js
@@ -1,6 +1,9 @@
 import ApolloClient from 'apollo-boost'
+import { InMemoryCache } from 'apollo-cache-inmemory';
 import { NavigationActions } from 'react-navigation'
 var _ = require('lodash')
+
+const cache = new InMemoryCache();
 
 class ApolloClientProvider {
 
@@ -9,6 +12,7 @@ class ApolloClientProvider {
       //uri: 'http://app.metodehayati.id:4000/graphql',
       uri: 'http://192.168.1.2:4001/graphql',
       // uri: 'http://192.168.44.129:4001/graphql',
+      cache,
       onError: this._onError,
     })
   }

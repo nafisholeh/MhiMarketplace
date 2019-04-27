9004008581a6fc7368c2542cee4c221a37d764f6
Rachmad <rachmadnafisholeh@gmail.com>
2019-04-27 20:29:16 +0700

    feat(home): change to add product for stock opname user

--- App/Containers/Home/Home.js
+++ App/Containers/Home/Home.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
-import { func, string } from 'prop-types';
+import { func, string, bool } from 'prop-types';
 import { createStructuredSelector } from 'reselect';
 
 import ApolloClientProvider from 'Services/ApolloClientProvider';
@@ -9,29 +9,38 @@ import { FETCH_CART } from 'GraphQL/Cart/Query';
 import List from 'Containers/Product/List';
 import { HeaderButton } from 'Components';
 import CartActions from 'Redux/CartRedux';
-import { getUserId } from 'Redux/SessionRedux';
+import { getUserId, isStokOpname } from 'Redux/SessionRedux';
 import { Images, Metrics } from 'Themes';
 import styles from './Styles';
     
 class Home extends Component {
   
   static navigationOptions = ({navigation}) => {
-    const {params = {}} = navigation.state
+    const {params: { isStokOpname = false } = {}} = navigation.state
     return {
       title: 'MHI.id',
       headerLeft: null,
       headerRight: (
         <HeaderButton
-          onPress={() => navigation.navigate('Cart')}
-          icon={Images.cart}
+          onPress={() => navigation.navigate(isStokOpname ? 'AddressInput' : 'Cart')}
+          icon={isStokOpname ? Images.plus : Images.cart}
         />
       ),
     }
   }
   
+  componentWillMount() {
+    this.passNavigationProps();
+  }
+  
   componentDidMount() {
     this.fetchInitCart();
   }
+  
+  passNavigationProps = () => {
+    const { navigation: { setParams }, isStokOpname } = this.props;
+    setParams({ isStokOpname });
+  };
   
   fetchInitCart = () => {
     const { storeCart, userId } = this.props;
@@ -61,10 +70,12 @@ class Home extends Component {
 Home.propTypes = {
   userId: string,
   storeCart: func,
+  isStokOpname: bool,
 }
 
 const mapStateToProps = createStructuredSelector({
   userId: getUserId(),
+  isStokOpname: isStokOpname(),
 });
 
 const mapDispatchToProps = (dispatch) => ({

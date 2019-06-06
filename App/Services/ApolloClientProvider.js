bc048d02df8cae28c7d6a977b667414c97073a27
Rachmad <rachmadnafisholeh@gmail.com>
2019-06-07 05:59:11 +0700

    feat(graphql): combine http-link and apollo-upload-client setup

--- App/Services/ApolloClientProvider.js
+++ App/Services/ApolloClientProvider.js
@@ -3,29 +3,41 @@ import { InMemoryCache } from 'apollo-cache-inmemory';
 import { HttpLink } from 'apollo-link-http';
 import { onError } from 'apollo-link-error';
 import { ApolloLink } from 'apollo-link';
+import { createUploadLink } from 'apollo-upload-client';
 import { NavigationActions } from 'react-navigation';
 
 import { InAppNotification } from 'Lib';
 
 const cache = new InMemoryCache();
 
-class ApolloClientProvider {
+const options = {
+  uri: 'http://app-dev.metodehayati.id:4001/graphql'
+};
 
+const httpLink = ApolloLink.from([
+  onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors) {
+      InAppNotification.error('Maaf, terjadi kesalahan', 'Terjadi kesalahan teknis, silahkan kontak pengembang');
+    } else if (networkError) {
+      InAppNotification.error('Maaf, terjadi kesalahan', 'Silahkan coba lagi atau tunggu beberapa saat');
+    }
+  }),
+  new HttpLink({
+    uri: 'http://app-dev.metodehayati.id:4001/graphql',
+    credentials: 'same-origin'
+  })
+]);
+
+const aggregatedLink = ApolloLink.split(
+  operation => operation.getContext().hasUpload,
+  createUploadLink(options),
+  httpLink
+);
+
+class ApolloClientProvider {
   constructor() {
     this.client = new ApolloClient({
-      link: ApolloLink.from([
-        onError(({ graphQLErrors, networkError }) => {
-          if (graphQLErrors) {
-            InAppNotification.error('Maaf, terjadi kesalahan', 'Terjadi kesalahan teknis, silahkan kontak pengembang');
-          } else if (networkError) {
-            InAppNotification.error('Maaf, terjadi kesalahan', 'Silahkan coba lagi atau tunggu beberapa saat');
-          }
-        }),
-        new HttpLink({
-          uri: 'http://app-dev.metodehayati.id:4001/graphql',
-          credentials: 'same-origin'
-        })
-      ]),
+      link: aggregatedLink,
       cache: new InMemoryCache()
     });
   }

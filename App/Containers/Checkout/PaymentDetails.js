39ce8e03b8c6387be64da72d53748f90fe2e294a
Rachmad <rachmadnafisholeh@gmail.com>
2019-06-18 15:43:09 +0700

    style(checkout): rename Harga Awal into Harga Total

--- App/Containers/Checkout/PaymentDetails.js
+++ App/Containers/Checkout/PaymentDetails.js
@@ -114,12 +114,12 @@ class PaymentDetails extends Component {
         }}
       >
         <View style={styles.paymentDetail}>
-          <Text>Harga Awal</Text>
+          <Text>Harga Total</Text>
           <Text>{parseToRupiah(grossPrice) || '-'}</Text>
         </View>
         <View style={styles.paymentDetail}>
           <Text>Harga Kurir</Text>
-          <Text>{`+ ${parseToRupiah(courierCost)}` || '-'}</Text>
+          <Text>{parseToRupiah(courierCost) || '-'}</Text>
         </View>
         {totalDiscount ? (
           <View style={styles.paymentDetail}>

c6ba573fcd2944d0b5c3f7e0ec4ec62dd6daa8d2
Rachmad <rachmadnafisholeh@gmail.com>
2019-06-20 19:50:35 +0700

    refactor(courier): optimize get upcoming shipping scedule

--- App/Lib/Shipping.js
+++ App/Lib/Shipping.js
@@ -3,26 +3,31 @@ import moment from 'moment';
 import { getIntervalDateToday } from './Date';
 
 export const getUpcomingShippingSched = (shippingDates = []) => {
-  const now = new Date();
-  let closest = Infinity;
-  let closestIndex = 0;
-
-  shippingDates.forEach((d, i) => {
-    const { date: itemDate } = d || {};
-     var date = new Date(itemDate);
-     if (date >= now && (date < new Date(closest) || date < closest)) {
-        closest = itemDate;
+  let closestShipping = null;
+  
+  if (shippingDates.length === 1) {
+    closestShipping = shippingDates[0];
+  } else if (shippingDates.length > 1) {
+    const now = new Date();
+    let closest = Infinity;
+    let closestIndex = 0;
+    shippingDates.forEach((d, i) => {
+      const { date: itemDate } = d || {};
+      let date = new Date(itemDate);
+      if (Number(date) >= Number(now) && (date <= new Date(closest) || date <= closest)) {
+        closest = date;
         closestIndex = i;
-     }
-  });
+      }
+    });
+    closestShipping = closestShipping = shippingDates.length > closestIndex ? shippingDates[closestIndex] : null;
+  }
 
-  const closestShipping = shippingDates.length > closestIndex ? shippingDates[closestIndex] : null;
   return exports.getReadableShippingSched(closestShipping);
 }
 
 export const getReadableShippingSched = shippingDate => {
   const { date, time_start = '00:00', time_end = '24:00' } = shippingDate || {};
-  if (!date || !shippingDate) return 'Jadwal tidak tersedia'; 
+  if (!date || !shippingDate) return 'Jadwal belum ada'; 
   
   const relativeDate = getIntervalDateToday(moment(date, 'YYYY-MM-DD'), 'DD MMM');
   let timeRange = '';

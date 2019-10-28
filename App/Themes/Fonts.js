2efbb0d5d54819957ac8d7bb10a77dad63016ff2
Rachmad <rachmadnafisholeh@gmail.com>
2019-10-28 22:53:37 +0700

    style: update all fonts with a default letter spacing

--- App/Themes/Fonts.js
+++ App/Themes/Fonts.js
@@ -5,36 +5,43 @@ export default {
     fontFamily: 'CircularStd-Bold',
     fontSize: 20,
     color: 'rgba(0,0,0,0.8)',
+    letterSpacing: 0.3,
   },
   TITLE_HEADER__BOLD: {
     fontFamily: 'CircularStd-Bold',
     fontSize: 20,
     color: Colors.black,
+    letterSpacing: 0.3,
   },
   TITLE_HEADER__NORMAL: {
     fontFamily: 'CircularStd-Book',
     fontSize: 18,
     color: 'rgba(0,0,0,0.68)',
+    letterSpacing: 0.3,
   },
   TITLE_HEADER__SMALL: {
     fontFamily: 'CircularStd-Book',
     fontSize: 16,
     color: 'rgba(0,0,0,0.3)',
+    letterSpacing: 0.3,
   },
   TITLE_BOLD: {
     fontFamily: 'CircularStd-Bold',
     fontSize: 16,
     color: Colors.black,
+    letterSpacing: 0.3,
   },
   TITLE_NORMAL: {
     fontFamily: 'CircularStd-Book',
     fontSize: 14,
     color: 'rgba(0,0,0,0.68)',
+    letterSpacing: 0.3,
   },
   TITLE_SMALL: {
     fontFamily: 'CircularStd-Book',
     fontSize: 12,
     color: 'rgba(0,0,0,0.3)',
+    letterSpacing: 0.3,
   },
   INSTRUCTION: {
     fontFamily: 'CircularStd-Book',

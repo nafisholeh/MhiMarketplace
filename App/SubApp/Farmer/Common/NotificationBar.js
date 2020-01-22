ad9fcd06fea930b758e82a8126bad567c923eb8a
Rachmad <rachmadnafisholeh@gmail.com>
2020-01-23 06:26:53 +0700

    feat(notif): display dotted bell if there is unseen notif

--- App/SubApp/Farmer/Common/NotificationBar.js
+++ App/SubApp/Farmer/Common/NotificationBar.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
-import { TouchableOpacity, Image } from 'react-native';
+import { TouchableOpacity, Image, View, Text } from 'react-native';
 import { withNavigation } from 'react-navigation';
+import { Query } from 'react-apollo';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
 
-import { Images, Metrics } from 'Themes';
+import { Images, Metrics, Colors } from 'Themes';
+import { NOTIFICATION_UNSEEN_TOTAL } from 'GraphQL/Notification/Query';
+import { getUserId } from 'Redux/SessionRedux';
+import { moderateScale } from 'Lib';
 
 class NotificationBar extends Component {
   openNotificationPage = () => {
@@ -11,22 +17,52 @@ class NotificationBar extends Component {
   };
 
   render() {
-    const { style } = this.props;
+    const { style, userId } = this.props;
     return (
-      <TouchableOpacity
-        onPress={this.openNotificationPage}
-        style={style}
+      <Query
+        query={NOTIFICATION_UNSEEN_TOTAL}
+        variables={{ user_id: userId }}
       >
-        <Image
-          source={Images.notification}
-          style={{
-            width: Metrics.icons.medium,
-            height: Metrics.icons.medium
-          }}
-        />
-      </TouchableOpacity>
+        {({ loading, error, data, refetch }) => {
+          const { userNotificationUnseenTotal: unseenTotal = 0 } = data || {};
+          return (
+            <View style={{...style, ...{ position: 'relative'}}}>
+              <TouchableOpacity
+                onPress={this.openNotificationPage}
+              >
+                <Image
+                  source={Images.notification}
+                  style={{
+                    width: Metrics.icons.medium,
+                    height: Metrics.icons.medium
+                  }}
+                />
+              </TouchableOpacity>
+              {unseenTotal
+                ? (
+                  <View
+                    style={{
+                      position: 'absolute',
+                      top: 0, right: 0,
+                      width: moderateScale(12),
+                      height: moderateScale(12),
+                      borderRadius: moderateScale(6),
+                      backgroundColor: Colors.veggie_dark,
+                    }}
+                  />
+                )
+                : null
+              }
+            </View>
+          );
+        }}
+      </Query>
     );
   }
 }
 
-export default withNavigation(NotificationBar);
+const mapStateToProps = createStructuredSelector({
+  userId: getUserId(),
+});
+
+export default connect(mapStateToProps, null)(withNavigation(NotificationBar));

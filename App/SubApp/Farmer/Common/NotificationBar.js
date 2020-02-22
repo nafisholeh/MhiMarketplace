b528622edfba755bd306ea892a3a1843db971da7
Rachmad <rachmadnafisholeh@gmail.com>
2020-02-23 06:48:27 +0700

    chore: test husky and stage-lint

--- App/SubApp/Farmer/Common/NotificationBar.js
+++ App/SubApp/Farmer/Common/NotificationBar.js
@@ -1,58 +1,51 @@
-import React, { Component } from 'react';
-import { TouchableOpacity, Image, View, Text } from 'react-native';
-import { withNavigation } from 'react-navigation';
-import { Query } from 'react-apollo';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { Component } from "react";
+import { TouchableOpacity, Image, View, Text } from "react-native";
+import { withNavigation } from "react-navigation";
+import { Query } from "react-apollo";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { Images, Metrics, Colors } from 'Themes';
-import { NOTIFICATION_UNSEEN_TOTAL } from 'GraphQL/Notification/Query';
-import { getUserId } from 'Redux/SessionRedux';
-import { moderateScale } from 'Lib';
+import { Images, Metrics, Colors } from "Themes";
+import { NOTIFICATION_UNSEEN_TOTAL } from "GraphQL/Notification/Query";
+import { getUserId } from "Redux/SessionRedux";
+import { moderateScale } from "Lib";
 
 class NotificationBar extends Component {
   openNotificationPage = () => {
     const { navigation } = this.props;
-    navigation.navigate('NotificationHistory');
+    navigation.navigate("NotificationHistory");
   };
 
   render() {
     const { style, userId } = this.props;
     return (
-      <Query
-        query={NOTIFICATION_UNSEEN_TOTAL}
-        variables={{ user_id: userId }}
-      >
-        {({ loading, error, data, refetch }) => {
+      <Query query={NOTIFICATION_UNSEEN_TOTAL} variables={{ user_id: userId }}>
+        {({ loading, error, data }) => {
           const { userNotificationUnseenTotal: unseenTotal = 0 } = data || {};
           return (
-            <View style={{...style, ...{ position: 'relative'}}}>
-              <TouchableOpacity
-                onPress={this.openNotificationPage}
-              >
+            <View style={{ ...style, ...{ position: "relative" } }}>
+              <TouchableOpacity onPress={this.openNotificationPage}>
                 <Image
                   source={Images.notification}
                   style={{
                     width: Metrics.icons.medium,
-                    height: Metrics.icons.medium
+                    height: Metrics.icons.medium,
                   }}
                 />
               </TouchableOpacity>
-              {unseenTotal
-                ? (
-                  <View
-                    style={{
-                      position: 'absolute',
-                      top: 0, right: 0,
-                      width: moderateScale(12),
-                      height: moderateScale(12),
-                      borderRadius: moderateScale(6),
-                      backgroundColor: Colors.veggie_dark,
-                    }}
-                  />
-                )
-                : null
-              }
+              {unseenTotal ? (
+                <View
+                  style={{
+                    position: "absolute",
+                    top: 0,
+                    right: 0,
+                    width: moderateScale(12),
+                    height: moderateScale(12),
+                    borderRadius: moderateScale(6),
+                    backgroundColor: Colors.veggie_dark,
+                  }}
+                />
+              ) : null}
             </View>
           );
         }}

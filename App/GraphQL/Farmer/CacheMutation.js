ec260dbdc17504c918edfc1c065f4b4e7497863b
Rachmad <rachmadnafisholeh@gmail.com>
2020-02-23 05:22:26 +0700

    fix(notif): omit redundant comment from notif tray

--- App/GraphQL/Farmer/CacheMutation.js
+++ App/GraphQL/Farmer/CacheMutation.js
@@ -1,119 +1,100 @@
-import ApolloClientProvider from 'Services/ApolloClientProvider'
-import { FETCH_FARMER_POST } from './Query';
+import ApolloClientProvider from "Services/ApolloClientProvider";
+import { FETCH_FARMER_POST } from "./Query";
 
-export const cacheNewComment = async (data) => {
-    const {
-        postId,
-        commentId,
-        content,
-        author,
-        dateCommented
-    } = data || {};
-    const {
-        _id: authorId,
-        ktp_photo_face_thumbnail: ktp_photo_face,
-        ktp_name
-    } = author || {};
-    const result = await ApolloClientProvider.client.query({
-        query: FETCH_FARMER_POST,
-        variables: { _id: postId }
-    });
-    const { data: postCache } = result || {};
-    const { farmerPost } = postCache || {};
-    const { comments = [] } = farmerPost || {};
+export const cacheNewComment = async data => {
+  const { postId, commentId, content, author, dateCommented } = data || {};
+  const { _id: authorId, ktp_photo_face_thumbnail: ktp_photo_face, ktp_name } =
+    author || {};
+  const result = await ApolloClientProvider.client.cache.readQuery({
+    query: FETCH_FARMER_POST,
+    variables: { _id: postId }
+  });
+  const { data: postCache } = result || {};
+  const { farmerPost } = postCache || {};
+  const { comments = [] } = farmerPost || {};
 
-    if (!Array.isArray(comments)) return;
-    const newComment = {
-        _id: commentId,
-        author: {
-            _id: authorId,
-            ktp_name,
-            ktp_photo_face,
-            __typename: "UserFarmer"
-        },
-        content,
-        date_commented: dateCommented,
-        likes: [],
-        likes_total: 0,
-        content_reply: [],
-        __typename: "FarmerComment"
-    };
-
-    ApolloClientProvider.client.writeQuery({
-        query: FETCH_FARMER_POST,
-        variables: { _id: postId },
-        data: {
-            farmerPost: {
-                ...farmerPost,
-                ...{
-                    comments: [].concat(comments, newComment)
-                }
-            }
+  if (!Array.isArray(comments)) return;
+  const newComment = {
+    _id: commentId,
+    author: {
+      _id: authorId,
+      ktp_name,
+      ktp_photo_face,
+      __typename: "UserFarmer"
+    },
+    content,
+    date_commented: dateCommented,
+    likes: [],
+    likes_total: 0,
+    content_reply: [],
+    __typename: "FarmerComment"
+  };
+  ApolloClientProvider.client.writeQuery({
+    query: FETCH_FARMER_POST,
+    variables: { _id: postId },
+    data: {
+      farmerPost: {
+        ...farmerPost,
+        ...{
+          comments: [].concat(comments, newComment)
         }
-    });
+      }
+    }
+  });
 };
 
-export const cacheNewReplyComment = async (data) => {
-    const {
-        postId,
-        commentId,
-        subCommentId,
-        content,
-        author,
-        dateCommented
-    } = data || {};
-    const {
-        _id: authorId,
-        ktp_photo_face_thumbnail: ktp_photo_face,
-        ktp_name
-    } = author || {};
-    const result = await ApolloClientProvider.client.query({
-        query: FETCH_FARMER_POST,
-        variables: { _id: postId }
-    });
-    const { data: postCache } = result || {};
-    const { farmerPost } = postCache || {};
-    const { comments = [] } = farmerPost || {};
-    
-    if (!Array.isArray(comments)) return;
-    const matchCommentIndex = comments.findIndex(({ _id }) => _id === commentId);
-    if (matchCommentIndex < 0) return;
-    const { content_reply: oldContentReply } = comments[matchCommentIndex];
-    const newContentReply = {
-        _id: subCommentId,
-        author: {
-            _id: authorId,
-            ktp_name,
-            ktp_photo_face,
-            __typename: "UserFarmer"
-        },
-        content,
-        date_commented: dateCommented,
-        likes: [],
-        likes_total: 0,
-        __typename: "FarmerCommentReply"
-    };
-    const newComment = {
-        ...comments[matchCommentIndex],
+export const cacheNewReplyComment = async data => {
+  const { postId, commentId, subCommentId, content, author, dateCommented } =
+    data || {};
+  const { _id: authorId, ktp_photo_face_thumbnail: ktp_photo_face, ktp_name } =
+    author || {};
+  const result = await ApolloClientProvider.client.cache.readQuery({
+    query: FETCH_FARMER_POST,
+    variables: { _id: postId }
+  });
+  const { data: postCache } = result || {};
+  const { farmerPost } = postCache || {};
+  const { comments = [] } = farmerPost || {};
+
+  if (!Array.isArray(comments)) return;
+  const matchCommentIndex = comments.findIndex(({ _id }) => _id === commentId);
+  if (matchCommentIndex < 0) return;
+  const { content_reply: oldContentReply } = comments[matchCommentIndex];
+  const newContentReply = {
+    _id: subCommentId,
+    author: {
+      _id: authorId,
+      ktp_name,
+      ktp_photo_face,
+      __typename: "UserFarmer"
+    },
+    content,
+    date_commented: dateCommented,
+    likes: [],
+    likes_total: 0,
+    __typename: "FarmerCommentReply"
+  };
+  const newComment = {
+    ...comments[matchCommentIndex],
+    ...{
+      content_reply: [].concat(oldContentReply, newContentReply)
+    }
+  };
+
+  ApolloClientProvider.client.writeQuery({
+    query: FETCH_FARMER_POST,
+    variables: { _id: postId },
+    data: {
+      farmerPost: {
+        ...farmerPost,
         ...{
-            content_reply: [].concat(oldContentReply, newContentReply)
+          comments: [
+            comments.slice(0, matchCommentIndex),
+            newComment,
+            comments.slice(matchCommentIndex + 1, comments.length)
+          ]
         }
-    };
-    
-    ApolloClientProvider.client.writeQuery({
-        query: FETCH_FARMER_POST,
-        variables: { _id: postId },
-        data: {
-            farmerPost: {
-                ...farmerPost,
-                ...{
-                    comments: [
-                        comments.slice(0, matchCommentIndex),
-                        newComment,
-                        comments.slice(matchCommentIndex + 1, comments.length)
-                    ]
-                }
-            }
-        }
-    });
+      }
+    }
+  });
 };
--- App/SubApp/Farmer/Containers/Socmed/NewsFeedDetail.js
+++ App/SubApp/Farmer/Containers/Socmed/NewsFeedDetail.js
@@ -1,60 +1,40 @@
-import React, { Component, Fragment } from 'react';
-import { View, ScrollView } from 'react-native';
-import { createStructuredSelector } from 'reselect';
-import { connect } from 'react-redux';
-import { withNavigation } from 'react-navigation';
-import { Query } from 'react-apollo';
-import { wrapScrollView } from 'react-native-scroll-into-view';
+import React, { Component, Fragment } from "react";
+import { View, ScrollView } from "react-native";
+import { createStructuredSelector } from "reselect";
+import { connect } from "react-redux";
+import { withNavigation } from "react-navigation";
+import { Query } from "react-apollo";
+import { wrapScrollView } from "react-native-scroll-into-view";
 
-import ApolloClientProvider from 'Services/ApolloClientProvider';
-import {
-  NewsFeedContent,
-  CommentItem,
-  CommentInput,
-} from './Components';
-import NewsFeedComments from './NewsFeedComments';
-import { Colors } from 'Themes';
-import { moderateScale, getUTCDate } from 'Lib';
-import { getSelectedListId } from 'Redux/ListRedux';
-import { getUser, getUserId, getUserPhoto } from 'Redux/SessionRedux';
-import { FETCH_FARMER_POST } from 'GraphQL/Farmer/Query';
+import ApolloClientProvider from "Services/ApolloClientProvider";
+import { NewsFeedContent, CommentItem, CommentInput } from "./Components";
+import NewsFeedComments from "./NewsFeedComments";
+import { Colors } from "Themes";
+import { moderateScale, getUTCDate } from "Lib";
+import { getSelectedListId } from "Redux/ListRedux";
+import { getUser, getUserId, getUserPhoto } from "Redux/SessionRedux";
+import { FETCH_FARMER_POST } from "GraphQL/Farmer/Query";
 import {
   COMMENT_TO_POST,
   REPLY_TO_COMMENT,
   cacheCommentReply,
-  cacheSubCommentReply,
-} from 'GraphQL/Farmer/Mutation';
-import { QueryEffectPage } from 'Components';
-import Config from 'Config/AppConfig';
+  cacheSubCommentReply
+} from "GraphQL/Farmer/Mutation";
+import { QueryEffectPage } from "Components";
+import Config from "Config/AppConfig";
 
 const CustomScrollView = wrapScrollView(ScrollView);
 
 class NewsFeedDetail extends Component {
-  static navigationOptions = ({navigation}) => ({ header: null })
-  
+  static navigationOptions = ({ navigation }) => ({ header: null });
+
   state = {
     commentId: null,
     commentAuthorId: null,
     isReplyComment: false,
-    parentUsername: '',
+    parentUsername: ""
   };
-  
-  componentDidMount() {
-    this.handleDataFetch();
-  }
-  
-  componentDidUpdate(prevProps) {
-    const { feedId } = this.props;
-    if (prevProps.feedId !== feedId) {
-      this.handleDataFetch();
-    }
-  }
-  
-  handleDataFetch = () => {
-    const { feedId } = this.props;
-    
-  };
-  
+
   replyComment = (feedId, authorId, name) => {
     this.setState({
       commentId: feedId,
@@ -63,16 +43,16 @@ class NewsFeedDetail extends Component {
       parentUsername: name
     });
   };
-  
+
   cancelReplyComment = () => {
     this.setState({
       commentId: null,
       commentAuthorId: null,
       isReplyComment: false,
-      parentUsername: ''
+      parentUsername: ""
     });
   };
-  
+
   submitCommentToPost = (feedId, feedAuthorId, comment) => {
     const { isReplyComment, commentId, commentAuthorId } = this.state;
     const { loggedInUserId, userData } = this.props;
@@ -82,89 +62,82 @@ class NewsFeedDetail extends Component {
       {
         content: comment,
         date_commented: getUTCDate(),
-        reply_author_id: loggedInUserId,
+        reply_author_id: loggedInUserId
       },
       isReplyComment
         ? {
-          post_id: feedId,
-          comment_id: commentId,
-          comment_author_id: commentAuthorId,
-        }
+            post_id: feedId,
+            comment_id: commentId,
+            comment_author_id: commentAuthorId
+          }
         : {
-          post_id: feedId,
-          post_author_id: feedAuthorId,
-        }
+            post_id: feedId,
+            post_author_id: feedAuthorId
+          }
     );
-    ApolloClientProvider.client.mutate({
-      mutation: isReplyComment ? REPLY_TO_COMMENT : COMMENT_TO_POST,
-      variables: { data },
-      update: (cache, data) => 
-        isReplyComment
-          ? cacheSubCommentReply(cache, data, feedId, commentId, comment)
-          : cacheCommentReply(cache, data, feedId, comment),
-      optimisticResponse: 
-        isReplyComment
-        ? {
-          replyCommentAsFarmer: {
-            _id: Math.random() * 1000 * -1,
-            content: comment,
-            date_commented: getUTCDate(),
-            author: {
-              _id: loggedInUserId,
-              ktp_nik,
-              ktp_name,
-              __typename: "UserFarmer"
-            },
-            likes_total: 0,
-            likes: [],
-            comment: [],
-            __typename: "replyCommentAsFarmer"
-          }
+    ApolloClientProvider.client
+      .mutate({
+        mutation: isReplyComment ? REPLY_TO_COMMENT : COMMENT_TO_POST,
+        variables: { data },
+        update: (cache, data) =>
+          isReplyComment
+            ? cacheSubCommentReply(cache, data, feedId, commentId, comment)
+            : cacheCommentReply(cache, data, feedId, comment),
+        optimisticResponse: isReplyComment
+          ? {
+              replyCommentAsFarmer: {
+                _id: Math.random() * 1000 * -1,
+                content: comment,
+                date_commented: getUTCDate(),
+                author: {
+                  _id: loggedInUserId,
+                  ktp_nik,
+                  ktp_name,
+                  __typename: "UserFarmer"
+                },
+                likes_total: 0,
+                likes: [],
+                comment: [],
+                __typename: "replyCommentAsFarmer"
+              }
+            }
+          : {
+              commentAsFarmer: {
+                _id: Math.random() * 1000 * -1,
+                content: comment,
+                date_commented: getUTCDate(),
+                author: {
+                  _id: loggedInUserId,
+                  ktp_nik,
+                  ktp_name,
+                  __typename: "UserFarmer"
+                },
+                likes_total: 0,
+                likes: [],
+                comments_total: 0,
+                content_reply: [],
+                __typename: "commentAsFarmer"
+              }
+            }
+      })
+      .then(res => {})
+      .catch(err => {})
+      .finally(() => {
+        if (isReplyComment) {
+          this.cancelReplyComment();
         }
-        : {
-          commentAsFarmer: {
-            _id: Math.random() * 1000 * -1,
-            content: comment,
-            date_commented: getUTCDate(),
-            author: {
-              _id: loggedInUserId,
-              ktp_nik,
-              ktp_name,
-              __typename: "UserFarmer"
-            },
-            likes_total: 0,
-            likes: [],
-            comments_total: 0,
-            content_reply: [],
-            __typename: "commentAsFarmer"
-          }
-        }
-    })
-    .then(res => {
-      
-    })
-    .catch(err => {
-      
-    })
-    .finally(() => {
-      if (isReplyComment) {
-        this.cancelReplyComment();
-      }
-    })
+      });
   };
 
   render() {
     const { navigation, feedId, loggedInUserId, userPhoto } = this.props;
     const { isReplyComment, parentUsername } = this.state;
     if (!feedId) {
-      return <View></View>
+      return <View></View>;
     }
     return (
-      <View style={{flex: 1}}>
-        <Query
-          query={FETCH_FARMER_POST}
-          variables={{ _id: feedId }}
-        >
+      <View style={{ flex: 1 }}>
+        <Query query={FETCH_FARMER_POST} variables={{ _id: feedId }}>
           {({ loading, error, data, refetch }) => {
             const { farmerPost: dataList } = data || {};
             if (dataList) {
@@ -178,7 +151,8 @@ class NewsFeedDetail extends Component {
                 likes,
                 likes_total
               } = dataList || {};
-              const { _id: feedAuthorId, ktp_name, ktp_photo_face } = author || {};
+              const { _id: feedAuthorId, ktp_name, ktp_photo_face } =
+                author || {};
               return (
                 <Fragment>
                   <CustomScrollView
@@ -209,27 +183,27 @@ class NewsFeedDetail extends Component {
                   </CustomScrollView>
                   <View
                     style={{
-                      position: 'absolute',
+                      position: "absolute",
                       bottom: 0,
                       left: 0,
                       right: 0,
                       backgroundColor: Colors.white,
                       paddingHorizontal: moderateScale(10),
                       paddingTop: moderateScale(5),
-                      paddingBottom: moderateScale(10),
+                      paddingBottom: moderateScale(10)
                     }}
                   >
                     <CommentInput
                       style={{
                         marginLeft: 0,
                         marginRight: 0,
-                        marginBottom: 0,
+                        marginBottom: 0
                       }}
                       onSubmitComment={comment =>
                         this.submitCommentToPost(feedId, feedAuthorId, comment)
                       }
                       showInfo={isReplyComment}
-                      info={isReplyComment ? `Balas ke ${parentUsername}` : ''}
+                      info={isReplyComment ? `Balas ke ${parentUsername}` : ""}
                       onClosingInfo={this.cancelReplyComment}
                       photo={userPhoto}
                     />
@@ -250,19 +224,20 @@ class NewsFeedDetail extends Component {
           }}
         </Query>
       </View>
-    )
-  };
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
   feedId: getSelectedListId(),
   loggedInUserId: getUserId(),
   userData: getUser(),
-  userPhoto: getUserPhoto(),
+  userPhoto: getUserPhoto()
 });
 
-const mapDispatchToProps = dispatch => ({
-  
-});
+const mapDispatchToProps = dispatch => ({});
 
-export default connect(mapStateToProps, mapDispatchToProps)(withNavigation(NewsFeedDetail));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withNavigation(NewsFeedDetail));

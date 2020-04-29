6317052a782604a0f8140f20b113b58f5fcd3595
Rachmad <rachmadnafisholeh@gmail.com>
2020-04-30 01:42:53 +0700

    fix(signup): avoid crashing if area list is empty

--- App/Redux/SessionRedux.js
+++ App/Redux/SessionRedux.js
@@ -10,7 +10,7 @@ const { Types, Creators } = createActions({
   reset: null,
   storeSession: ["user", "user_type"],
   storeSignupEmail: ["email"],
-  storeSubAppSession: ["subAppSession"]
+  storeSubAppSession: ["subAppSession"],
 });
 
 export const SessionTypes = Types;
@@ -22,23 +22,23 @@ export const INITIAL_STATE = Immutable({
   user: null,
   user_type: null,
   signupEmail: null,
-  subAppSession: "user"
+  subAppSession: "user",
 });
 
 /* ------------- Selectors ------------- */
 
-export const sessionSelectors = () => state => state.session;
+export const sessionSelectors = () => (state) => state.session;
 
 export const getUser = () =>
-  createSelector(sessionSelectors(), state => state.user || null);
+  createSelector(sessionSelectors(), (state) => state.user || null);
 
 export const getMockUserId = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     return "5d93631029c05f7a60f247a5";
   });
 
 export const getUserId = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return null;
     const { _id } = user;
@@ -46,7 +46,7 @@ export const getUserId = () =>
   });
 
 export const getUserPhoto = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return null;
     const { ktp_photo_face } = user;
@@ -54,28 +54,32 @@ export const getUserPhoto = () =>
   });
 
 export const getUserShortBio = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return null;
     const { ktp_name, ktp_photo_face } = user;
     return {
       name: ktp_name || null,
-      photo: ktp_photo_face || null
+      photo: ktp_photo_face || null,
     };
   });
 
 export const getUserCommodities = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return null;
     const { areas = [] } = user;
-    const distinctCommodities = areas.reduce((distinctCommodities, item) => {
-      const { commodity_id } = item || {};
-      const { name } = commodity_id || {};
-      return distinctCommodities.some(({ name: nameTemp }) => name === nameTemp)
-        ? distinctCommodities
-        : [...distinctCommodities, commodity_id];
-    }, []);
+    const distinctCommodities = Array.isArray(areas)
+      ? areas.reduce((distinctCommodities, item) => {
+          const { commodity_id } = item || {};
+          const { name } = commodity_id || {};
+          return distinctCommodities.some(
+            ({ name: nameTemp }) => name === nameTemp
+          )
+            ? distinctCommodities
+            : [...distinctCommodities, commodity_id];
+        }, [])
+      : [];
     return distinctCommodities;
   });
 
@@ -90,7 +94,7 @@ export const getUserCommoditiesName = () =>
   );
 
 export const isAdmin = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     const { user_type } = user;
@@ -99,7 +103,7 @@ export const isAdmin = () =>
   });
 
 export const isReseller = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     const { user_type } = user;
@@ -108,7 +112,7 @@ export const isReseller = () =>
   });
 
 export const isStokOpname = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     const { user_type } = user;
@@ -117,7 +121,7 @@ export const isStokOpname = () =>
   });
 
 export const isKeuangan = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     const { user_type } = user;
@@ -126,7 +130,7 @@ export const isKeuangan = () =>
   });
 
 export const isKurir = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     const { user_type } = user;
@@ -135,7 +139,7 @@ export const isKurir = () =>
   });
 
 export const isFarmer = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user, user_type } = state;
     if (!user) return false;
     if (user_type && user_type === Config.userType.FARMER) return true;
@@ -143,21 +147,21 @@ export const isFarmer = () =>
   });
 
 export const isLoggedin = () =>
-  createSelector(sessionSelectors(), state => {
+  createSelector(sessionSelectors(), (state) => {
     const { user } = state;
     if (!user) return false;
     return true;
   });
 
 export const getSignupEmail = () =>
-  createSelector(sessionSelectors(), state => state.signupEmail);
+  createSelector(sessionSelectors(), (state) => state.signupEmail);
 
 export const getChosenSubApp = () =>
-  createSelector(sessionSelectors(), state => state.subAppSession);
+  createSelector(sessionSelectors(), (state) => state.subAppSession);
 
 /* ------------- Reducers ------------- */
 
-export const reset = state => INITIAL_STATE;
+export const reset = (state) => INITIAL_STATE;
 
 // store user session related data
 export const storeSession = (state, { user, user_type }) =>
@@ -175,5 +179,5 @@ export const reducer = createReducer(INITIAL_STATE, {
   [Types.RESET]: reset,
   [Types.STORE_SESSION]: storeSession,
   [Types.STORE_SIGNUP_EMAIL]: storeSignupEmail,
-  [Types.STORE_SUB_APP_SESSION]: storeSubAppSession
+  [Types.STORE_SUB_APP_SESSION]: storeSubAppSession,
 });

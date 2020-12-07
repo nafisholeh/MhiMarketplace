c10fa1392eb888556651cf723d137fe967e4dd42
Rachmad <rachmadnafisholeh@gmail.com>
2020-12-08 02:30:22 +0700

    feat: show email input on signin page

--- App/SubApp/DigitalVillage/Containers/Signin/Signin.js
+++ App/SubApp/DigitalVillage/Containers/Signin/Signin.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { StyleSheet, View, ImageBackground } from 'react-native';
 import { connect } from 'react-redux';
 import SplashScreen from 'react-native-splash-screen';
 
-import { InputText } from 'common-v3';
+import { InputTextWithShadow } from 'common-v3';
+import { withNoHeader } from 'Hoc';
+import { IMAGES, METRICS } from 'themes-v3';
+import { screenWidth } from 'Lib';
 
 class Signin extends Component {
   componentDidMount() {
@@ -11,13 +14,20 @@ class Signin extends Component {
   }
   render() {
     return (
-      <View>
-        <Text> index </Text>
-        <InputText title="Nomor KK" />
-        <InputText title="Nomor KK" isDisabled value="test" />
+      <View style={styles.container}>
+        <ImageBackground source={IMAGES.BG_ORANGE} style={styles.container}>
+          <InputTextWithShadow
+            title="Email"
+            containerPadding={METRICS.LARGE}
+            error="wajib diisi"
+          />
+        </ImageBackground>
       </View>
     );
   }
 }
 
-export default connect(null, null)(Signin);
+const styles = StyleSheet.create({
+  container: { flex: 1 },
+});
+export default connect(null, null)(withNoHeader(Signin));

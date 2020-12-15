f4eebf361b5cc85df5ab77a6c91186206b176635
Rachmad <rachmadnafisholeh@gmail.com>
2020-12-16 01:56:35 +0700

    feat(ktpsuccess): implement design

--- App/Navigation/DigitalVillage/Stack/Account.js
+++ App/Navigation/DigitalVillage/Stack/Account.js
@@ -4,6 +4,7 @@ import Signin from 'digital-village/Containers/Signin/Signin';
 import KtpSearch from 'digital-village/Containers/Signup/KtpSearch';
 import KtpConfirmation from 'digital-village/Containers/Signup/KtpConfirmation';
 import KtpTutorial from 'digital-village/Containers/Signup/KtpTutorial';
+import PhotoSuccess from 'digital-village/Containers/Signup/PhotoSuccess';
 import PhotoFailure from 'digital-village/Containers/Signup/PhotoFailure';
 
 const AccountNav = createStackNavigator(
@@ -12,10 +13,11 @@ const AccountNav = createStackNavigator(
     KtpSearch: { screen: KtpSearch },
     KtpConfirmation: { screen: KtpConfirmation },
     KtpTutorial: { screen: KtpTutorial },
+    PhotoSuccess: { screen: PhotoSuccess },
     PhotoFailure: { screen: PhotoFailure },
   },
   {
-    initialRouteName: 'PhotoFailure',
+    initialRouteName: 'PhotoSuccess',
   }
 );
 
